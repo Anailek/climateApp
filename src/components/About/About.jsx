713cf5151ba0024137ff0fe858f774201d2091c6
Anailek <eruizarg@gmail.com>
2021-09-07 20:13:26 -0300

    fixed about

---
+++ src/components/About/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../About/About.css'
+
+function About() {
+    return (
+    <a className="about" href="https://www.linkedin.com/in/elianaruiz91/"> contact </a>
+    )
+}
+
+export default About;
