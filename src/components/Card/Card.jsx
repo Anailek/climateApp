a416a210a86e0eb950f7d4864f07b3327448c262
Anailek <eruizarg@gmail.com>
2021-09-07 20:58:17 -0300

    add Card components. Fixed about. add button img

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,31 +1,45 @@
-import React from 'react';
-import './Card.css';
-import { Link } from 'react-router-dom';
-export default function Card({ min, max, name, img, onClose, id }) {
+import React from "react";
+import { Link } from "react-router-dom";
+import button from "../button.png";
+import "./Card.css";
+
+function Card({ min, max, name, img, onClose, id }) {
   return (
     <div className="card">
-      <div id="closeIcon" className="row">
-        <button onClick={onClose} className="btn btn-sm btn-danger">X</button>
+      <div className="button_container">
+        <button onClick={onClose}>
+          <img className="button" src={button} alt="button" />
+        </button>
       </div>
-      <div className="card-body">
-        <Link to={`/ciudad/${id}`} >
+
+      <div>
+        <Link to={`/ciudad/${id}`}>
           <h5 className="card-title">{name}</h5>
         </Link>
-        <h5 className="card-title">{name}</h5>
-        <div className="row">
-          <div className="col-sm-4 col-md-4 col-lg-4">
+      </div>
+
+      <div className="card-body">
+        <div className="temp">
+          <div>
             <p>Min</p>
             <p>{min}°</p>
           </div>
-          <div className="col-sm-4 col-md-4 col-lg-4">
+
+          <div>
             <p>Max</p>
             <p>{max}°</p>
           </div>
-          <div className="col-sm-4 col-md-4 col-lg-4">
-            <img className="iconoClima" src={"http://openweathermap.org/img/wn/" + img + "@2x.png"} width="80" height="80" alt="" />
-          </div>
+        </div>
+
+        <div>
+          <img
+            src={"http://openweathermap.org/img/wn/" + img + "@2x.png"}
+            alt="Img"
+          />
         </div>
       </div>
     </div>
   );
-};
+}
+
+export default Card;
