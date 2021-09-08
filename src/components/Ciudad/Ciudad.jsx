97f6783345733ffa9f1a005bc2b8c6803b02fc15
Anailek <eruizarg@gmail.com>
2021-09-07 21:09:33 -0300

    ciudad changes

--- src/components/Ciudad/Ciudad.jsx
+++ src/components/Ciudad/Ciudad.jsx
@@ -1,22 +1,23 @@
-import React from 'react';
+import React from "react";
+import "../Ciudad/Ciudad.css";
 
-export default function Ciudad({city}) {
-    if(!city){
-        return <div>City not found</div>
-    }
-    return (
-        <div className="ciudad">
-                <div className="container">
-                    <h2>{city.name}</h2>
-                    <div className="info">
-                        <div>Temperatura: {city.temp} ºC</div>
-                        <div>Clima: {city.weather}</div>
-                        <div>Viento: {city.wind} km/h</div>
-                        <div>Cantidad de nubes: {city.clouds}</div>
-                        <div>Latitud: {city.latitud}º</div>
-                        <div>Longitud: {city.longitud}º</div>
-                    </div>
-            </div>
+function Ciudad({ city }) {
+  return (
+    <div className="container">
+      <div className="city">
+        <div>
+          <h2>{city.name}</h2>
+          <div className="info">
+            <div>Temperatura: {city.temp} ºC</div>
+            <div>Clima: {city.weather}</div>
+            <div>Viento: {city.wind} km/h</div>
+            <div>Cantidad de nubes: {city.clouds}</div>
+            <div>Latitud: {city.latitud}º</div>
+            <div>Longitud: {city.longitud}º</div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
+export default Ciudad;
